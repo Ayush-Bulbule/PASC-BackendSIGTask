2638ab4f015c865fb1ef649d9ad522557d0efbe5
Ayush-Bulbule <ayushbulbule24@gmail.com>
2024-02-06 19:03:40 +0530

    Initial Project

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express"
+
+
+
+const app = express();
+
+//to parse the request body
+app.use(express.json());
+
+//
+
+//Books Api Routes: GET, POST, PUT, DELETE
+let books = [
+    {
+        id: 1,
+        name: "Harry Potter",
+        author: "J.K. Rowling"
+    },
+    {
+        id: 2,
+        name: "The Alchemist",
+    },
+    {
+        id: 3,
+        name: "The Da Vinci Code",
+    }
+
+];
+
+
+app.get("/ping", (req, res) => {
+    res.status(200).json({ message: "Pong" });
+})
+
+
+// Add other requests GET, POST, PUT, DELETE
+app.listen(8000, () => {
+    console.log(`App is live on: http://localhost:8000`);
+});
